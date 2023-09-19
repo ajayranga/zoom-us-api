f689507d7107fb4abecf47ebf44d8b11a5408b39
Ajay <ajay.ranga@hcode.tech>
2023-09-19 10:44:04 +0530

    env setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        ZOOM_US_CLIENT_ID: Joi.string().min(5),
+        ZOOM_US_CLIENT_SECRET: Joi.string().min(5),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
